11a2af7511c718276f173749c7044bcc4a463ea7
RenattoPadilha <55454860+RenattoPadilha@users.noreply.github.com>
2026-01-31 21:11:07 -0300

    feat: character sprites and animation

--- Assets/Scripts/PlayerMovement.gd
+++ Assets/Scripts/PlayerMovement.gd
@@ -2,9 +2,9 @@ extends Node
 
 class_name MovementComponent
 
-#@export var speed: float = 200
-@export var lines: Array[float] = [192, 0, -192]
+@export var lines: Array[float] = [143, 17, -143]
 @export var line_change_speed: float = 10
+@onready var anim = $"../AnimatedSprite2D"
 
 var body: CharacterBody2D
 var curr_lane_index: int
@@ -23,15 +23,18 @@ func process_movement(delta: float) -> void:
 
 func _handle_mov_input():
 	if Input.is_action_just_released("move_up"):
+		anim.play("up")
 		curr_lane_index = min(curr_lane_index+1, lines.size()-1)
 	if Input.is_action_just_released("move_down"):
+		anim.play("down")
 		curr_lane_index = max(curr_lane_index-1, 0)
 
 func _apply_movement(delta: float):
-	#body.velocity.x = speed
-	
-	#changing line
+
 	var target_y = lines[curr_lane_index]
+	if abs(body.position.y - target_y) < 1.0:
+		if anim.animation != "run":
+			anim.play("run")
 	body.position.y = lerp(body.position.y, target_y, line_change_speed * delta)
 	
 	body.move_and_slide()
